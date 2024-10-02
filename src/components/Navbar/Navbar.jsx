ad1b7394cce1dc14bd93ece84869edec2dd92b40
SankalpKadam <sankalptp07@gmail.com>
2024-10-02 00:50:16 -0500

    Implemented basic navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <p className='navbar__logo'>PathFinding <span>Visualizer</span></p>
+      <div className='navbar__options'>
+        <select name="algorithm" id="select__algo">
+          <option value="BFS">Breadth-First Search</option>
+          <option value="BFS">Depth-First Search</option>
+          <option value="BFS">Dijkstra's</option>
+        </select>
+        <p>Reset</p>
+        <p>Clear Walls</p>
+        <button>Visualize!</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
