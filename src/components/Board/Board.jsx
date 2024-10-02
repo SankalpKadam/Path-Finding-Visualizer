0ba671cc8235f0e471866619359fb5f2e96e5528
SankalpKadam <sankalptp07@gmail.com>
2024-10-02 02:35:21 -0500

    Placed start and end node

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,33 +2,30 @@ import React, { useEffect, useState } from "react";
 import './index.css'
 import Node from "../Node/Node";
 const Board = () =>{
-    const {rows, cols} = {rows: 10, cols:5}
     const [nodes, setNodes] = useState([])
     useEffect(()=>{
         let nodes = []
-        for(let i = 0; i< 10; i++){
+        for(let i = 0; i< 15; i++){
             let temp = []
-            for (let j =0 ; j< 5; j++){
+            for (let j =0 ; j< 36; j++){
                 temp.push(0)
             }
             nodes.push(temp)
         }
         setNodes(nodes)
-        console.log("nodes ",nodes);
         
     },[])
     return (
         <div className="board">
             {
-                // nodes.map((row, rowIndex)=>{
-                //     return (<>
-                //     {
+                nodes.map((row, rowIndex)=>{
+                    return (<>
+                    {
 
-                //         row.map((col, colIndex)=><Node/>)
-                //     }
-                //     </>)
-                // })
-                Array(108*5).fill(0).map((item)=><Node/>)
+                        row.map((col, colIndex)=><Node row={rowIndex} col={colIndex}/>)
+                    }
+                    </>)
+                })
             }
         </div>
     )
--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './index.css'
-const Node = () => {
-    return (
-        <div className='node'>
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import StartNode from '../Elements/StartNode';
+import AdjustIcon from '@mui/icons-material/Adjust';
+import TargetNode from '../Elements/TargetNode';
 
+const Node = ({row, col}) => {
+    const handleClick = (e)=>{
+        console.log(e.target.id);
+        
+    }
+    return (
+        <div className='node' id={`${row}-${col}`} onClick={handleClick}>
+            {
+                (row === 11 && col === 6)  ? <StartNode/> :
+                (row === 14 && col === 5) && <TargetNode/>
+            }
         </div>
     )
 }
