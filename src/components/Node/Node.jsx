0b3dee95ed57fffe41f67a126ef9bbab4da302de
SankalpKadam <sankalptp07@gmail.com>
2024-10-03 01:53:21 -0500

    Click to set the start and end

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './index.css'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StartNode from '../Elements/StartNode';
 import AdjustIcon from '@mui/icons-material/Adjust';
 import TargetNode from '../Elements/TargetNode';
-import { useSelector } from 'react-redux';
-import { startNode, targetNode } from '../../store/slices/nodeSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { setStartNode, setTargetNode, startNode, targetNode } from '../../store/slices/nodeSlice';
 
 const Node = ({row, col}) => {
     const start = useSelector(startNode)
     const target = useSelector(targetNode)
-    const handleClick = (e)=>{
-        console.log(e.target.id);
+    const [type, setType] = useState('')
+    const dispatch = useDispatch();
+    const handleClick = ()=>{
+        if(!start){
+            setType('start')
+            // myRef.current.appendChild(<StartNode/>)
+            dispatch(setStartNode({
+                start:[row, col]
+            }))
+        }
+        else if(!target){
+            setType('target')
+            // myRef.current.appendChild(<TargetNode/>)
+            dispatch(setTargetNode({
+                target:[row,col]
+            }))
+        }
+        myRef.current.onClick = ()=>{}
         
     }
+    const myRef = useRef()
     return (
-        <div className='node' id={`${row}-${col}`} onClick={handleClick}>
+        <div className='node' id={`${row}-${col}`} ref={myRef} onClick={handleClick} onDrop={()=>{
+            console.log("helllo");
+            
+        }} onDragOver={()=>{
+            console.log('over');
+            
+        }}
+        >
+
             {
-                (row === start[0] && col === start[1])  ? <StartNode/> :
-                ((row === target[0] && col === target[1]) && <TargetNode/>)
+                (type==='start') ? <StartNode/>:(type === 'target' && <TargetNode/>)
             }
         </div>
     )
